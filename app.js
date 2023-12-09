720314ad94bf64318cb08cbc17305adde9d7a229
Adrián Santos Mena <adrian.santos.mena.contractor@bbva.com>
2023-12-09 15:52:07 +0100

    [DEV] Added get-cv endpoint.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
@@ -13,4 +14,15 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/get-cv", (req, res) => {
+	let cvName = req.query.file_name;
+
+	var file = fs.createReadStream("CV_2023.pdf");
+	var stat = fs.statSync("CV_2023.pdf");
+	res.setHeader("Content-Length", stat.size);
+	res.setHeader("Content-Type", "application/pdf");
+	res.setHeader("Content-Disposition", `attachment; filename=${cvName}.pdf`);
+	file.pipe(res);
 });
