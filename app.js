2c10df7e6441aa7790765aeb9235bd005e5577aa
Adrián Santos Mena <adrian.santos.mena.contractor@bbva.com>
2023-12-09 16:14:25 +0100

    Testing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ app.use(cors());
 
 app.get("/", (req, res) => {
 	res.type("application/json");
+	console.log(path.basename);
 	res.json({ text: "Testing Hello World! in Vercel" });
 });
 
